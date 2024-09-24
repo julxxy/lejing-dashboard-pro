0154f5ef84379af9d82e403e8f8dadcd0f6f89e0
julian <julxxy@outlook.com>
2024-09-24 19:24:38 +0800

    feat:Update

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { isDebugEnable } from './common/debugEnable.ts'
+import { logger } from './common/logger.ts'
+
+if (isDebugEnable) {
+  logger.debug('Debug mode is enabled.')
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
