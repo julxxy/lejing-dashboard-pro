2bfa751fdd582fe0d7b3704075f25e2998d1f8ba
julian <julxxy@outlook.com>
2024-09-24 18:21:29 +0800

    feat:Update

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+const list = [1, 2, 3, 4, 5].map(item => item * 2)
+console.log(list)
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
