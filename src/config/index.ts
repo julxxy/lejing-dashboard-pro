8f83371cf199c959cab74ed27fc7fc7d584fbef0
julian <julxxy@outlook.com>
2024-09-29 23:33:32 +0800

    feat: Add helper function library

---
+++ src/config/index.ts
@@ -0,0 +1,45 @@
+/**
+ * Runtime configuration for the application.
+ * @file Configuration file for the application.
+ * @author Weasley
+ */
+type ENV = 'dev' | 'test' | 'prod'
+
+const env: ENV = (document.documentElement.getAttribute('data-env') as ENV) || 'test'
+const config = {
+  dev: {
+    mock: true,
+    debug: true,
+    baseURI: '/api',
+    cdn: 'https://cdn-dev.example.com',
+    apiUrl: 'http://localhost:3000',
+    authUrl: 'http://localhost:3000/auth',
+    uploadUrl: 'http://localhost:3000/upload',
+    mockApiUrl: 'https://dev.example.com/mock'
+  },
+  test: {
+    mock: false,
+    debug: true,
+    baseURI: '/api',
+    cdn: 'https://cdn-test.example.com',
+    apiUrl: 'https://test.example.com',
+    authUrl: 'https://test.example.com/auth',
+    uploadUrl: 'https://test.example.com/upload',
+    mockApiUrl: 'https://test.example.com/mock'
+  },
+  prod: {
+    mock: false,
+    debug: false,
+    baseURI: '/api',
+    cdn: 'https://cdn-prod.example.com',
+    apiUrl: 'https://www.example.com',
+    authUrl: 'https://www.example.com/auth',
+    uploadUrl: 'https://www.example.com/upload'
+  }
+}
+
+// Export the configuration object.
+export default {
+  env,
+  ...config[env]
+}
---
+++ src/utils/index.ts
@@ -0,0 +1,28 @@
+/**
+ * Helper function library
+ */
+
+// Format CNY money amount
+export const formatMoneyCNY = (amount: string | number | bigint): string => {
+  const formater = new Intl.NumberFormat('zh-Hans-CN', {
+    style: 'currency',
+    currency: 'CNY'
+  })
+
+  const numericAmount = typeof amount === 'string' ? parseFloat(amount) : Number(amount)
+  return isNaN(numericAmount) ? 'Invalid amount' : formater.format(numericAmount)
+}
+
+
+// Format date to date string
+export const formatDateToLocalString = (date?: Date, pattern?: 'yyyy-MM-dd' | 'HH:mm:ss' | 'yyyy-MM-dd HH:mm:ss') => {
+  const _date = date ? date : new Date()
+  switch (pattern) {
+    case 'yyyy-MM-dd':
+      return _date.toLocaleDateString().replace(/\//g, '-')
+    case 'HH:mm:ss':
+      return _date.toLocaleTimeString()
+    default:
+      return _date.toLocaleString().replace(/\//g, '-')
+  }
+}
