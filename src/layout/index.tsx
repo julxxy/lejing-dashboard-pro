b54f86ba107e86e084f54315352aa276dbfe8393
julian <julxxy@outlook.com>
2024-10-04 23:20:25 +0800

    feat: update left side menu fold&unfold

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,14 +1,13 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Layout, Watermark } from 'antd'
 import NaviHeader from '@/components/NaviHeader'
 import NavFooter from '@/components/NavFooter'
 import LeftSideMenu from '@/components/SideMenu'
-import { Outlet } from 'react-router-dom'
-import Welcome from '@/views/wlecome'
 import styles from '@/layout/index.module.less'
 import api from '@/api'
 import useZustandStore from '@/store/useZustandStore.ts'
-import Demo from '@/views/Demo.tsx'
+import Welcome from '@/views/wlecome'
+import { Outlet } from 'react-router-dom'
 
 const { Content } = Layout
 const watermarkContent = import.meta.env.VITE_APP_ID as string
@@ -25,17 +24,31 @@ const LayoutFC: React.FC = () => {
     getUserInfo()
   }, [])
 
+  const [contentHeight, setContentHeight] = useState<string>('100vh') // 默认视口高度
+
+  // 动态计算内容区域的的高度
+  useEffect(() => {
+    const headerHeight = 64 // 假设导航栏高度为 64px
+    const updateHeight = () => {
+      const newHeight = window.innerHeight - headerHeight
+      setContentHeight(`${newHeight}px`)
+    }
+    updateHeight() // 初始化时计算高度
+    window.addEventListener('resize', updateHeight) // 监听窗口大小变化
+    return () => {
+      window.removeEventListener('resize', updateHeight) // 清理事件监听器
+    }
+  }, [])
+
   return (
     <Watermark content={[watermarkContent]}>
       <Layout style={{ minHeight: '100vh' }}>
         <LeftSideMenu />
         <Layout ref={wrapperRef} className={styles.rightContentArea}>
           <NaviHeader />
-          <Content className={styles.content}>
+          <Content className={styles.content} style={{ height: contentHeight }}>
             <div className={styles.wrapper}>
               <Outlet context={<Welcome />} />
-              {/*<Demo/>  内容超出 <Content/> 区域，撑最外层的，我希望滚动条出现在 <Content> 区域*/}
-              <Demo />
             </div>
           </Content>
           <NavFooter />
