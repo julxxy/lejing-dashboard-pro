216aece1a58daa60d6bf5382aed18b03510a2a09
julian <julxxy@outlook.com>
2024-10-18 16:57:54 +0800

    feat:Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import '@/App.css'
 import { RouterProvider } from 'react-router-dom'
-import { App as AntdApp, ConfigProvider } from 'antd'
+import { App as AntdApp, ConfigProvider, theme } from 'antd'
 import { router } from '@/router'
 import AntdGlobalProvider, { useThemeToken } from '@/context/AntdGlobalProvider.ts'
 import zhCN from 'antd/lib/locale/zh_CN'
@@ -8,13 +8,22 @@ import enUS from 'antd/lib/locale/en_US'
 import { Environment } from '@/types/appEnum.ts'
 import 'dayjs/locale/zh-cn'
 import dayjs from 'dayjs'
+import useZustandStore from '@/store/useZustandStore.ts'
 
 if (Environment.isLocaleCN) dayjs.locale('zh-cn')
 
 function App() {
+  const { isDarkEnable } = useZustandStore()
+
   return (
     <>
-      <ConfigProvider theme={{ ...useThemeToken }} locale={Environment.isLocaleCN ? zhCN : enUS}>
+      <ConfigProvider
+        theme={{
+          ...useThemeToken,
+          algorithm: isDarkEnable ? theme.darkAlgorithm : theme.defaultAlgorithm,
+        }}
+        locale={Environment.isLocaleCN ? zhCN : enUS}
+      >
         <AntdApp>
           <AntdGlobalProvider />
           <RouterProvider router={router} />
