01b2f7bddf6ea16b256b86d123dd5f56cb3126a7
weasley <weasley2023@outlook.com>
2024-10-29 09:42:35 +0800

    feat: Update

--- src/context/EChartsManager.ts
+++ src/context/EChartsManager.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 import * as echarts from 'echarts'
 import { ECharts, EChartsType } from 'echarts'
 import { RefObject } from 'react'
@@ -12,7 +11,7 @@ export const EChartsManager = {
    */
   getInstanceIfPresent: (chartRef: RefObject<HTMLDivElement>): ECharts | null => {
     if (!chartRef.current) {
-      console.warn('React 组件的 ECharts ref 对象不存在')
+      log.warn('React 组件的 ECharts ref 对象不存在')
       return null
     }
     // 尝试获取现有的 ECharts 实例，如果不存在则创建一个新的实例
